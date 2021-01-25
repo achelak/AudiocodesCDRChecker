84e01080e095a4b30fa6eb2d3ac88df9d2fdf833
Artem <neimor95@gmail.com>
2021-01-25 20:03:36 +0200

    was added new env var - postgres_host

--- vendor/connect.php
+++ vendor/connect.php
@@ -1,10 +1,11 @@
 <?php
+    $host = getenv('POSTGRES_HOST');
     $dbname = getenv('POSTGRES_DB');
     $user = getenv('POSTGRES_USER');
     $password = getenv('POSTGRES_PASSWORD');
 
     // $connect = mysqli_connect('db', 'root', 'madenci', 'CDR');
-    $connect = pg_connect("host=db dbname=$dbname user=$user password=$password");
+    $connect = pg_connect("host=$host dbname=$dbname user=$user password=$password");
 
     if (!$connect) {
         die('Error connect to DataBase');
